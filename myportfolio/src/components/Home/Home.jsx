315f03820a064cc3572024e7fa194754b7692c34
ashwiii11 <ashwithaacharya11@gmail.com>
2025-11-13 07:29:33 +0530

    home

---
+++ myportfolio/src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="text-white flex w-full justify-between items-start p-10 md:p-20">
+        <div className='md:w-2/4 md:pt-10'>
+        <h1 className='text-3x1 md:text-6x1 font-bold flex leading-normal tracking-tigher'>Hello' I am Ashwitha</h1>
+        <p className='text-sm md:text-2xl tracking-tight'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
