5b2defd1e9e0d3e64c42faf84dd1cc862d8ec994
ashwiii11 <ashwithaacharya11@gmail.com>
2025-11-13 07:16:01 +0530

    navbar

---
+++ myportfolio/src/components/navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { RiMenu2Line, RiCloseLine } from '@remixicon/react';
+
+const Navbar = () => {
+  const [menu, openMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
+
+  return (
+    <div>
+      <nav className="flex flex-wrap justify-between md:items-center text-white px-6 md:px-20">
+        
+        <span className="text-xl font-bold tracking-wide">Portfolio</span>
+
+        <ul
+          className={`${
+            menu ? "block" : "hidden"
+          } mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
+        >
+          <a href="#about">
+            <li className="text-md transition-all duration-300 p-1 md:p-0">
+              About
+            </li>
+          </a>
+
+          <a href="#skills">
+            <li className="text-md transition-all duration-300 p-1 md:p-0">
+              Skills
+            </li>
+          </a>
+
+          <a href="#projects">
+            <li className="text-md transition-all duration-300 p-1 md:p-0">
+              Projects
+            </li>
+          </a>
+
+          <a href="#contact">
+            <li className="text-md transition-all duration-300 p-1 md:p-0">
+              Contact
+            </li>
+          </a>
+        </ul>
+
+        {/* Mobile Menu Button */}
+        {showMenu ? (
+          <RiMenu2Line
+            size={30}
+            className="md:hidden absolute right-10 top-6 transition-all duration-300"
+            onClick={() => {
+              openMenu(true);
+              setShowMenu(false);
+            }}
+          />
+        ) : (
+          <RiCloseLine
+            size={30}
+            className="md:hidden absolute right-10 top-6 transition-all duration-300"
+            onClick={() => {
+              openMenu(false);
+              setShowMenu(true);
+            }}
+          />
+        )}
+
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
